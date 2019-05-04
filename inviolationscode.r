3e09c61e66cfb0aa7f703d629a793f360273421c
Erick Young <ebernardyoung@gmail.com>
2019-05-04 14:38:43 -0400

    Edited inviolationscode.r

--- inviolationscode.r
+++ inviolationscode.r
@@ -1,5 +1,18 @@
-attach(Food_Inspections)
-summary(SCORE)
-mean(SCORE)
-attach(Food_Inspection_Violations)
-summary(POINTVALUE)
+getwd()
+## library tidyverse
+library(tidyverse)
+
+## function to store readable data to the variable 'inspections'
+inspections <- read.csv("Food_inspections.csv", TRUE, ",")
+
+
+## function to store readable data to the variable 'violations'
+violations <- read.csv("Food_Inspection_Violations.csv", TRUE, ",")
+
+
+## function to join the readable data by their HSISID number columns
+all <- inspections %>%
+  inner_join(violations, by="HSISID")
+
+## glimpse at the HSISID results of the readable data 
+glimpse(all)
